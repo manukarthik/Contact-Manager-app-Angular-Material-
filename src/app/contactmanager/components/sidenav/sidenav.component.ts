eb3458fbe730b6e1bd1a247fc59170f8a3e0a9e3
manukarthik <manukarthikkr@gmail.com>
2018-04-24 15:04:45 -0400

    Creating Layout Component

--- src/app/contactmanager/components/sidenav/sidenav.component.ts
+++ src/app/contactmanager/components/sidenav/sidenav.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, NgZone } from '@angular/core';
+const SMALL_WIDTH_BREAKPOINT=720;
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.css']
 })
 export class SidenavComponent implements OnInit {
-
-  constructor() { }
+  private mediaMatcher: MediaQueryList = matchMedia(`(max-width: ${SMALL_WIDTH_BREAKPOINT}px)`)
+  constructor(zone : NgZone) {
+    this.mediaMatcher.addListener(mql => 
+    zone.run(()=> this.mediaMatcher= mql)
+    )
+   }
 
   ngOnInit() {
   }
 
+  isScreenSmall():boolean{
+    return this.mediaMatcher.matches;
+  }
 }
